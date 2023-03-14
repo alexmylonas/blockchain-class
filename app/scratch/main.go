79ad71cc744b648a79a42c72927d39c659b09792
Alex Mylonas <alex.a.mylonas@gmail.com>
2023-03-14 21:06:36 +0200

    scracth

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -22,14 +22,16 @@ func main() {
 	}
 }
 func run() error {
+
+	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
+	if err != nil {
+		return fmt.Errorf("failed to load private key: %w", err)
+	}
+
 	tx := Tx{
 		FromID: 1,
 		ToID:   2,
 		Amount: 1000,
-	}
-	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
-	if err != nil {
-		return fmt.Errorf("failed to load private key: %w", err)
 	}
 
 	data, err := json.Marshal(tx)
@@ -37,16 +39,26 @@ func run() error {
 		return fmt.Errorf("failed to marshal transaction: %w", err)
 	}
 
-	v := crypto.Keccak256Hash(data)
+	v := crypto.Keccak256(data)
 
-	sig, err := crypto.Sign(v.Bytes(), privateKey)
+	sig, err := crypto.Sign(v, privateKey)
 	if err != nil {
 		return fmt.Errorf("failed to sign data: %w", err)
 	}
 
 	s := hexutil.Encode(sig)
-	fmt.Println(string(s))
+	fmt.Println("SIG:", string(s))
 
+	// ==================================================
+	// OVER THE WIRE
+
+	publicKey, err := crypto.SigToPub(v, sig)
+	if err != nil {
+		return fmt.Errorf("failed to recover public key: %w", err)
+	}
+
+	address := crypto.PubkeyToAddress(*publicKey).String()
+	fmt.Println("PUB:", address)
 	return nil
 
 }
