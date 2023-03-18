db330ae6ef0bbf9349065ea14ed4f01cbeff9df1
Alex Mylonas <alex.a.mylonas@gmail.com>
2023-03-18 15:02:57 +0200

    peer api

---
+++ foundation/blockchain/peer/peer.go
@@ -0,0 +1,73 @@
+package peer
+
+import "sync"
+
+type Peer struct {
+	Host string
+}
+
+func New(host string) *Peer {
+	return &Peer{
+		Host: host,
+	}
+}
+
+func (p Peer) Match(host string) bool {
+	return p.Host == host
+}
+
+type PeerStatus struct {
+	LatestBlockHash string `json:"latest_block_hash"`
+	LatestBlockNum  uint64 `json:"latest_block_num"`
+	KnownPeers      []Peer `json:"known_peers"`
+}
+
+type PeerSet struct {
+	mu  sync.RWMutex
+	set map[Peer]struct{}
+}
+
+func NewPeerSet() *PeerSet {
+	return &PeerSet{
+		set: make(map[Peer]struct{}),
+	}
+}
+
+func (ps *PeerSet) Add(peer Peer) bool {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+
+	_, exists := ps.set[peer]
+	if !exists {
+		ps.set[peer] = struct{}{}
+		return true
+	}
+
+	return false
+}
+
+func (ps *PeerSet) Remove(peer Peer) bool {
+	ps.mu.Lock()
+	defer ps.mu.Unlock()
+
+	_, exists := ps.set[peer]
+	if exists {
+		delete(ps.set, peer)
+		return true
+	}
+
+	return false
+}
+
+func (ps *PeerSet) Copy(host string) []Peer {
+	ps.mu.RLock()
+	defer ps.mu.RUnlock()
+
+	peers := make([]Peer, 0, len(ps.set)-1)
+	for peer := range ps.set {
+		if peer.Host != host { // Removing self
+			peers = append(peers, peer)
+		}
+	}
+	return peers
+}
